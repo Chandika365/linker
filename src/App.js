fc11c012d8a47c030d8d521a20d5dd6b35247ff4
chandika365 <chandikajayasanka20010706@gmail.com>
2023-11-05 09:41:15 +0530

    test commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import Careers from './Components/Careers';
+import Catagory from './Components/Catagory';
+import Connectlinker from './Components/Connectlinker';
+import Contact from './Components/Contact';
+import Featuresection from './Components/Featuresection';
+import Header from './Components/Header';
+import Herosection from './Components/Herosection';
+import Logocloud from './Components/Logocloud';
+import Navigationbar from './Components/Navigationbar';
+import Product from './Components/Product';
+import Producttestominals from './Components/Producttestominals';
+import Promosection from './Components/Promosection';
+import State from './Components/State';
+import Teamsection from './Components/Teamsection';
+import Testomionals from './Components/Testomionals';
+
+
+
+function App() {
+  return (
+    <>
+    {/* <Navigationbar/> */}
+    {/* <Header/> */}
+    <Herosection/>
+    <Promosection/>
+    <State/>
+    <Logocloud/>
+    <Featuresection/>
+    <Testomionals/>
+    <Teamsection/>
+    <Product/>
+    <Connectlinker/>
+    <Producttestominals/>
+    <Catagory/>
+    <Careers/>
+    <Contact/>
+    </>
+
+  );
+}
+
+export default App;
